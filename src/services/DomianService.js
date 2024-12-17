3ac97c9dad0f9056f3f2980e25581ad1e7a445b8
Rupesh Jha <rj187225@gmail.com>
2024-12-17 15:54:44 +0530

    feat : added ec2 uel in domain serice

--- src/services/DomianService.js
+++ src/services/DomianService.js
@@ -1,5 +1,7 @@
 const baseUrlDevlopment = "http://localhost:4000/";
 
+const ec2Url = "http://65.2.40.170:5000/"
+
 const baseUrlDev =
   "https://ia10swmere.execute-api.ap-south-1.amazonaws.com/dev/";
 
@@ -7,7 +9,7 @@ const baseUrlLive =
   "https://ia10swmere.execute-api.ap-south-1.amazonaws.com/prod/";
 
 const GetBaseUrl = () => {
-  return baseUrlDev;
+  return ec2Url;
 };
 
 export const DomainService = {
